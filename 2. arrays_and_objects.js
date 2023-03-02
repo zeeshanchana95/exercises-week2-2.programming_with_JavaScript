f54d0c45bfaa327d7dc6ed176a03cdc42a557d39
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2023-03-02 12:23:12 +0500

    Working on Arrays and Objects

---
+++ 2. arrays_and_objects.js
@@ -0,0 +1,56 @@
+/**
+ * Exercise: Creating arrays and objects
+In this exercise lab you will practice creating arrays and objects.
+
+Tasks to complete
+Create a new empty array literal and assign it to the variable clothes.
+
+Add 5 of your favorite items of clothing as strings using the push() method.
+
+Remove the fifth piece of clothing from the array using the pop() method.
+
+Add a new piece of clothing using the push() method.
+
+Use console.log to show the third item from the clothes array in the console.
+
+Create a new empty object literal and assign it to the variable favCar.
+
+Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+
+Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+
+Use the console to log the entire favCar object.
+ */
+
+
+//Task 01: Create a new empty array literal and assign it to the variable clothes.
+var clothes = [];
+
+
+//Task 02: Add 5 of your favorite items of clothing as strings using the push() method.
+clothes.push("T-Shirts");
+clothes.push("Salwaar");
+clothes.push("Kameez");
+clothes.push("Shirt")
+clothes.push("Jacket");
+
+//Task 03: Remove the fifth piece of clothing from the array using the pop() method.
+clothes.pop();
+
+//Task 04: Add a new piece of clothing using the push() method.
+clothes.push("Jeans");
+
+//Task 05: Use console.log to show the third item from the clothes array in the console.
+console.log(clothes[2]);
+
+//Task 06: Create a new empty object literal and assign it to the variable favCar.
+var favCar = {};
+
+//Task 07: Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+favCar.color = "pink";
+
+//Task 08: Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+favColor.covertible = true;
+
+//Task 09: Use the console to log the entire favCar object.
+console.log(favCar);
